a5e0b6b7e099f2562069f17d5fad8b329061ebc4
DinaAshraf97 <dinaashraf509@gmail.com>
2023-11-26 20:05:50 +0200

    Screenshot for failures

--- src/test/java/TestBase/TestBase.java
+++ src/test/java/TestBase/TestBase.java
@@ -1,11 +1,17 @@
 package TestBase;
 
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 import pages.BasePage;
 import pages.LoginPage;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.concurrent.TimeUnit;
 
 import static drivers.BrowserFactory.setBrowser;
@@ -25,7 +31,18 @@ public class TestBase {
     }
 
     @AfterMethod
-    public void quitDriver() {
+    public void recordFailure(ITestResult result){
+
+        if(ITestResult.FAILURE == result.getStatus())
+        {
+            var camera=(TakesScreenshot)driver;
+            File screenshot=camera.getScreenshotAs(OutputType.FILE);
+            try{
+                Files.move(screenshot.toPath(), new File("Resources/screenshot/" + result.getName() + ".png").toPath());
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
         driver.quit();
     }
 }
